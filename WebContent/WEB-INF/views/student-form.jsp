e909874f6378ede65b536da5f270729260cfa4d0
Yash Maheshwari <yashmaheshwari696@gmail.com>
2020-10-14 15:46:08 +0530

    basic mvc application

---
+++ WebContent/WEB-INF/views/student-form.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Student form</title>
+</head>
+<body>
+	<form action="submitForm" method="post">
+		<input type="text" name="name" placeholder="Enter your name.."/>
+		<button type="submit" value="Submit">Submit</button>
+	</form>
+</body>
+</html>
---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StudentFormController {
+
+	@RequestMapping("/showForm")
+	public ModelAndView showForm() {
+		ModelAndView mv = new ModelAndView("student-form");
+		return mv;
+	}
+
+	@RequestMapping("/submitForm")
+	public ModelAndView submitForm(@RequestParam(value = "name") String studentName) {
+		ModelAndView mv = new ModelAndView("student-info");
+		mv.addObject("studentName", studentName);
+		return mv;
+	}
+
+}
